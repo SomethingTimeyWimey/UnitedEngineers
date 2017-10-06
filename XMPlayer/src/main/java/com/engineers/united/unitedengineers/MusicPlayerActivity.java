ef2b3b61beae0733b04604cd4ba19266b157373f
SomethingTimeyWimey <vishwasmalhotra96@gmail.com>
2017-10-06 10:34:43 -0400

    MP3 player base
    made song, and songAdapter java classes and 2 layouts

---
+++ XMPlayer/src/main/java/com/engineers/united/unitedengineers/MusicPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.engineers.united.unitedengineers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musicplayeractivity);
+    }
+}
