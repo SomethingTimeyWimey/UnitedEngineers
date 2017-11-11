0c993758f11f1dab803962ce02f2d2c53f888028
AldoNdreu <31778237+AldoNdreu@users.noreply.github.com>
2017-11-10 22:36:37 -0500

    UpdatedLayoutWithXMPlayer

--- XMPlayer/src/main/java/com/engineers/united/unitedengineers/MainActivity.java
+++ XMPlayer/src/main/java/com/engineers/united/unitedengineers/MainActivity.java
@@ -1,81 +1,74 @@
 package com.engineers.united.unitedengineers;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.engineers.united.unitedengineers.R;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.engineers.united.unitedengineers.mFragments.FavoritesFragment;
+import com.engineers.united.unitedengineers.mFragments.RadioFragment;
+import com.engineers.united.unitedengineers.mFragments.SettingsFragment;
+import com.engineers.united.unitedengineers.mFragments.PinFragment;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            switch (item.getItemId()) {
-                case R.id.menu_radio:
-
-                    setTitle("Page One");
-                    PageOne fragment = new PageOne();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.content, fragment, "FragmentName");
-                    fragmentTransaction.commit();
-                    return true;
-
-                case R.id.menu_favorites:
-
-                    setTitle("Page Two");
-                    PageTwo fragment2 = new PageTwo();
-                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction2.replace(R.id.content, fragment2, "FragmentName");
-                    fragmentTransaction2.commit();
-                    return true;
-
-                case R.id.menu_settings:
-                    setTitle("Page Three");
-                    PageThree fragment3 = new PageThree();
-                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction3.replace(R.id.content, fragment3, "FragmentName");
-                    fragmentTransaction3.commit();
-                    return true;
-
-                case R.id.menu_pin:
-
-                    setTitle("Page Four");
-                    PageFour fragment4 = new PageFour();
-                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction4.replace(R.id.content, fragment4, "FragmentName");
-                    fragmentTransaction4.commit();
-                    return true;
-            }
-
-            return false;
-        }
-
-    };
-
+public class MainActivity extends AppCompatActivity implements AHBottomNavigation.OnTabSelectedListener{
+    AHBottomNavigation bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        bottomNavigation= (AHBottomNavigation) findViewById(R.id.myBottomNavigation_ID);
+        bottomNavigation.setOnTabSelectedListener(this);
+        this.createNavItems();
+    }
+    private void createNavItems()
+    {
+        //CREATE ITEMS
+        AHBottomNavigationItem radioItem=new AHBottomNavigationItem("Radio",R.drawable.radio);
+        AHBottomNavigationItem favoritesItem=new AHBottomNavigationItem("Favorites",R.drawable.favorites);
+        AHBottomNavigationItem settingsItem=new AHBottomNavigationItem("Settings",R.drawable.settings);
+        AHBottomNavigationItem pinItem=new AHBottomNavigationItem("Pin",R.drawable.pin);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //ADD ITEMS TO BAR
+        bottomNavigation.addItem(radioItem);
+        bottomNavigation.addItem(favoritesItem);
+        bottomNavigation.addItem(settingsItem);
+        bottomNavigation.addItem(pinItem);
 
-        getFragmentManager().beginTransaction()
-                .replace(R.id.container, new List())
-                .commit();
+        //PROPERTIES
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#191919"));
+        bottomNavigation.setCurrentItem(0);
+    }
+    @Override
+    public boolean onTabSelected(int position, boolean wasSelected) {
+        if(position==0)
+        {
+            RadioFragment radioFragment=new RadioFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_id,radioFragment).commit();
+        }else if(position==1)
+        {
+            FavoritesFragment favoritesFragment=new FavoritesFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_id,favoritesFragment).commit();
+        }else if(position==2)
+        {
+            SettingsFragment settingsFragment=new SettingsFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_id,settingsFragment).commit();
+        } else if(position==3)
+        {
+            PinFragment pinFragment=new PinFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_id,pinFragment).commit();
+        }
+        return true;
     }
 
     @Override
---
+++ XMPlayer/src/main/java/com/engineers/united/unitedengineers/mFragments/FavoritesFragment.java
@@ -0,0 +1,34 @@
+package com.engineers.united.unitedengineers.mFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.engineers.united.unitedengineers.R;
+/**
+ * Created by Oclemmy on 5/10/2016 for ProgrammingWizards Channel and http://www.Camposha.com.
+ */
+public class FavoritesFragment extends Fragment {
+    String[] favorite={"Favorites"};
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.favorites_fragment,container,false);
+        ListView lv= (ListView) rootView.findViewById(R.id.favoriteListView);
+        ArrayAdapter adapter=new ArrayAdapter(this.getActivity(),android.R.layout.simple_list_item_1,favorite);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), favorite[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+}
