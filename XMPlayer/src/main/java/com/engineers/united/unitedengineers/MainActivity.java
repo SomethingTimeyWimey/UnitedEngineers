96aec5100c7027d3023a5b706268b3deb830e33e
AldoNdreu <31778237+AldoNdreu@users.noreply.github.com>
2017-12-17 00:43:12 -0500

    RemovedUnusedImportStatementsFromAddedFunctionalityForFavouritesFragment

--- XMPlayer/src/main/java/com/engineers/united/unitedengineers/MainActivity.java
+++ XMPlayer/src/main/java/com/engineers/united/unitedengineers/MainActivity.java
@@ -7,7 +7,6 @@ package com.engineers.united.unitedengineers;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +15,6 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
-import com.engineers.united.unitedengineers.mFragments.FavoritesFragment;
 import com.engineers.united.unitedengineers.mFragments.FavouriteListFragment;
 import com.engineers.united.unitedengineers.mFragments.StationListFragment;
 
